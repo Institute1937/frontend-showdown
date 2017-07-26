108040c1ba0eeef9141d9ae78ba56952c2688c02
cerico <garethrobertlee@gmail.com>
2017-07-26 18:04:32 +0100

    ready for react and elm

---
+++ webpack.elm.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+function resolve (dir) {
+  return path.join(__dirname, '..', dir)
+}
+
+module.exports = options => {
+  return {
+    entry: './js/vue/index.js',
+    output: {
+      filename: 'bundle.js',
+    },
+        resolve: {
+      extensions: ['.js', '.vue', '.json'],
+      alias: {
+        'vue$': 'vue/dist/vue.esm.js',
+         '@': resolve('src')
+        }
+    },
+    devtool: 'source-map',
+    devServer: {
+      contentBase: path.resolve(__dirname, './'),
+      compress: true,
+      port: 2003,
+      historyApiFallback: {
+        index: 'vue.html',
+      },
+    },
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+      },{
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        // options: vueLoaderConfig
+      }],
+    },
+  }
+}
