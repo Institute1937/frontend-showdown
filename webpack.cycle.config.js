192c7e03e8b6900d009a0dbb43468cbeab6a9c83
cerico <garethrobertlee@gmail.com>
2017-07-27 21:22:24 +0100

    webpack prepared for cycle

--- webpack.cycle.config.js
+++ webpack.cycle.config.js
@@ -1,25 +1,37 @@
-const path = require('path');
+var path = require('path');
 
 
-module.exports = options => {
-  return {
-    entry: './js/cycle/index.js',
-    output: {
-      filename: 'bundle.js',
-    },
-    devtool: 'source-map',
-    devServer: {
-      contentBase: path.resolve(__dirname, './'),
-      compress: true,
-      port: 2005,
+module.exports = {
+  entry: 
+           ['./js/cycle/index.js']
+  ,
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel-loader'],
+        include: __dirname,
+        exclude: /node_modules/
+      }
+    ]
+  },
+  // plugins: ( ENV == 'production' ?
+  //            [
+  //             new webpack.optimize.UglifyJsPlugin({minimize: true}),
+  //            ]
+  //            :
+  //            [new webpack.HotModuleReplacementPlugin()]
+  // ),
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './',
+    // hot: true
+    port: 2007,
        historyApiFallback: {
         index: 'js/cycle/index.html',
       },
-    },
-    module: {
-      rules: [{
-        loader: 'babel-loader',
-      }],
-    },
   }
-}
+};
